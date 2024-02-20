25112f95bce4c7dc4428dbaf159f862679d86115
moodye4980 <160150623+moodye4980@users.noreply.github.com>
2024-02-20 00:18:21 -0500

    P1HW1
    pythons input and print, working with int and numbers

---
+++ P1HW1_MoodyEmmaa.py
@@ -0,0 +1,15 @@
+#Emma Moody
+#2/19/24
+#P1HW1
+#Python input and print functions
+
+num1 = input("Enter Integer: ")
+num1 = int(num1)
+print("You entered: ", num1)
+print(num1, "squared is", num1 **2)
+print("And", num1, "cubed is", num1 **3,"!!")
+
+num2 = input("Enter another integer: ")
+num2 = int(num2)
+print(num1, "+", num2, "is", num1 + num2)
+print(num1, "*", num2, "is", num1 * num2)
