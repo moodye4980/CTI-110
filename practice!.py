3b9dea33db03b7c7ca611d58afd2e087543e37cc
moodye4980 <160150623+moodye4980@users.noreply.github.com>
2024-02-27 11:29:43 -0500

    some practice!

---
+++ practice!.py
@@ -0,0 +1,40 @@
+#F-strings
+'''
+pay = 600
+heat = 50.25
+rent = 300
+
+print(f"Weekly Pay: ${pay:.2f}")
+print(f"Heat: ${heat:.2f}")
+print(f"Rent: ${rent:.2f}")
+
+print(f"Combined Bill: ${(heat + rent):.2f}")
+
+#division
+
+#standard division
+print(20/5)
+print(60.5/0.5)
+print(20/3)
+
+
+#Floor Division - return an integer
+print(20//3)
+print(20//4)
+
+#modlus - return as a float or integer
+print(100%8)
+print(100%92)
+print(20%2.5)
+
+num = 15
+if (num%2) == 0:
+    print(f"(num) is even.")
+else:
+    print(f"(num)is odd.")
+
+    print()
+'''
+#name = "Ian"
+
+print(f"Name: {'Ian'}")
