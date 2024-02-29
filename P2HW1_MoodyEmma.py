e399d9651d888116b0c527a62da16ee585128db8
moodye4980 <160150623+moodye4980@users.noreply.github.com>
2024-02-29 11:43:46 -0500

    P2HW1
    still in progress

---
+++ P2HW1_MoodyEmma.py
@@ -0,0 +1,16 @@
+#Emma Moody
+#2/29/24
+#P2HW1
+#Types of data and calucations
+
+print("This program calculates and displats travel expenses")
+budget = float(input("Enter Budget : "))
+destination = input("Enter your travel destination: ")
+gas = float(input("How much do you think you will spend on gas? "))
+accomodations = float(input("Approximately, how much will you need for accomodation/hotel? "))
+food = float(input("Approximately, how much do you need for food? "))
+
+print("---------Travel Expenses----------")
+print("Location:    " + destination)
+print(f"Initial Budget: ${budget:.2f}")
+    
