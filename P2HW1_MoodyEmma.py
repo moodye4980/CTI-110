c4a13dd6bdf519665394f34cacf1a923820e5616
moodye4980 <160150623+moodye4980@users.noreply.github.com>
2024-03-03 23:46:39 -0500

    P2HW1_MoodyEmma.py

--- P2HW1_MoodyEmma.py
+++ P2HW1_MoodyEmma.py
@@ -4,13 +4,20 @@
 #Types of data and calucations
 
 print("This program calculates and displats travel expenses")
-budget = float(input("Enter Budget : "))
-destination = input("Enter your travel destination: ")
-gas = float(input("How much do you think you will spend on gas? "))
-accomodations = float(input("Approximately, how much will you need for accomodation/hotel? "))
-food = float(input("Approximately, how much do you need for food? "))
+budget = float(input("\nEnter Budget : "))
+destination = input("\nEnter your travel destination: ")
+gas = float(input("\nHow much do you think you will spend on gas? "))
+accomodations = float(input("\nApproximately, how much will you need for accomodation/hotel? "))
+food = float(input("\nApproximately, how much do you need for food? "))
 
-print("---------Travel Expenses----------")
-print("Location:    " + destination)
-print(f"Initial Budget: ${budget:.2f}")
-    
+print("\n-----------Travel Expenses------------")
+print("Location:          " + destination)
+print(f"Initial Budget:    ${budget:.2f}")
+print(f"Fuel:              ${gas:.2f}")
+print(f"Accomodations:     ${accomodations:.2f}")
+print(f"Food:              ${food:.2f}")
+print("--------------------------------------")
+
+expenses = gas + accomodations + food
+
+print(f"Remaining Balance: ${budget - expenses:.2f}")
