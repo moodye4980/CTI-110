c39e0a749008ae8018d751dbb7b7bfeadf153e97
moodye4980 <160150623+moodye4980@users.noreply.github.com>
2024-04-11 11:32:16 -0400

    P5LAB
    in progress !

---
+++ P5LAB_MoodyEmma.py
@@ -0,0 +1,67 @@
+#Emma Mood
+#4/11/24
+#P5LAB
+#User-defined function
+
+#function to determine change returned to customer
+def disperse_change(change):
+
+    if change == 0:
+        print("No change is owed")
+
+    #calculate the amount of each coin needed
+    #interget division - //
+
+    num_dollars = change // 100
+    change = change - (num_dollars * 100)
+
+    num_quarters = change // 25
+    change = change - (num_quarters* 25)
+
+    num_dimes = change // 10
+    change = change - (num_dimes * 10)
+
+    num_nickels = change // 5
+    change = change - (num_nickels * 5)
+
+    num_pennies = change // 1
+
+    #display coins owed
+
+    if num_dollars > 0:
+        print(num_dollars, end = " ")
+        if num_dollars ==1:
+            print("Dollar")
+        else:
+            print("Dollars")
+
+    if num_quarters > 0:
+        print(num_quarters, end = " ")
+        if num_quarters == 1:
+            print("Quarter")
+        else:
+            print("Quarters")
+            
+    if num_dimes > 0:
+        print(num_dimes, end = " ")
+        if num_dimes ==1:
+            print("Dime")
+        else: #if num_dimes > 1
+            print("Dimes")
+
+    if num_nickels > 0:
+        print(num_nickels, end = " ")
+        if num_nickels == 1:
+            print("Nickel")
+        else: 
+            print("Nickels")
+
+    if num_pennies > 0:
+        print(num_pennies, end = " ")
+        if num_pennies == 1:
+            print("Penny")
+        else: 
+            print("Pennies")
+
+#test it out. call it.
+disperse_change(1248)
